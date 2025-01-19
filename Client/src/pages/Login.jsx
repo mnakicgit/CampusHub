b70dc3e212a64125f74456d9cc4e68c2a4d1d0e4
mnakicgit <mnakic00@fesb.hr>
2025-01-19 22:18:00 +0100

    Refined login page

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Container, Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 
 function Login() {
 	return (
 		<>
-			<div style={{ position: "relative", top: "0", left: "0" }}>
-				<img src="src/assets/logoPozadina.png" alt="CampusHub logo" style={{ width: "10rem", position: "relative", top: "-5rem", left: "0px", zIndex: "5" }} />
-				<Card className="rounded-4 pt-5" style={{ minWidth: "22rem", position: "absolute", top: "0px", left: "0px" }}>
+			<div className="d-flex flex-column align-items-center" style={{ position: "relative", top: "0", left: "0" }}>
+				<img
+					src="src/assets/logoPozadina.png"
+					alt="CampusHub logo"
+					style={{ width: "10rem", position: "absolute", top: "-6rem", left: "123px", zIndex: "5", filter: "drop-shadow(0px 7px 7px rgba(1, 12, 24, 40%))" }}
+				/>
+				<Card className="rounded-4 pt-5" style={{ minWidth: "22rem", position: "relative", top: "0px", left: "0px", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)" }}>
 					<Card.Body>
 						<Form className="d-flex flex-column align-items-center">
 							<Form.Group className="mb-3 w-100 d-flex flex-column align-items-center" controlId="loginFormUsername">
@@ -17,15 +22,25 @@ function Login() {
 
 							<Form.Group className="mb-3 w-100 d-flex flex-column align-items-center" controlId="loginFormPassword">
 								<Form.Label>Password</Form.Label>
-								<Form.Control type="password" placeholder="Password" />
+								<Form.Control type="password" placeholder="Upiši password" />
 							</Form.Group>
 
-							<Button className="mt-3" variant="primary" type="submit">
+							<Button className="mt-2" variant="primary" type="submit">
 								Prijava
 							</Button>
 						</Form>
 					</Card.Body>
 				</Card>
+				<div className="mt-4 mb-2 w-100 d-flex justify-content-center align-items-center">
+					<Link to="/registracija" style={{ textDecoration: "none" }}>
+						<Button style={{ width: "9em", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)" }}>Registriraj se</Button>
+					</Link>
+					<span className="mx-2 text-white">ili</span>
+					<Link to="/" style={{ textDecoration: "none" }}>
+						<Button style={{ width: "9em", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)" }}>Nastavi kao gost</Button>
+					</Link>
+				</div>
+				<div className="text-white">Neprijavljeni korisnici nemaju pristup privatnom sadržaju</div>
 			</div>
 		</>
 	);
